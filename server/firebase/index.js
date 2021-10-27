7209b817ba8a7646b82ea62a48f4212b693ea2cf
= <=>
2021-10-27 17:51:17 +0530

    sever side

---
+++ server/firebase/index.js
@@ -0,0 +1,22 @@
+// var admin = require("firebase-admin");
+
+// var serviceAccount = require("../config/fbServiceAccountKey.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://ecommerce-225c8.firebaseio.com",
+// });
+
+// module.exports = admin;
+
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../config/fbServiceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+module.exports = admin;
+
